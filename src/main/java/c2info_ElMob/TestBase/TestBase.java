d3b3d8986bf2ee94139076293f9c0e9a4fa2da86
chetan <chetan.gmail.com>
2018-02-16 17:39:07 +0530

    2nd commit

--- src/main/java/c2info_ElMob/TestBase/TestBase.java
+++ src/main/java/c2info_ElMob/TestBase/TestBase.java
@@ -3,6 +3,7 @@ package c2info_ElMob.TestBase;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -19,8 +20,7 @@ public class TestBase {
 	
 	DesiredCapabilities capabilities = new DesiredCapabilities();
 	
-	public void DeviceCapabilities() throws MalformedURLException{
-		
+	public void DeviceCapabilities() throws MalformedURLException{	
 		capabilities.setCapability(CapabilityType.BROWSER_NAME,"ANDROID");
 		capabilities.setCapability(CapabilityType.VERSION,"5.1");
 		capabilities.setCapability("deviceName","ZX1D64CPT2");
@@ -28,9 +28,7 @@ public class TestBase {
 		capabilities.setCapability("appPackage","com.c2info.ecolite");
 		capabilities.setCapability("appActivity","com.c2info.ecolite.ui.activity.SplashScreenActivity");
 		driver = new AndroidDriver<WebElement>(new URL("http://127.0.0.1:4723/wd/hub"),capabilities);
-		//return new LoginPage();
-		
-		
+		//return new LoginPage();		
 	}
 	
 	public void hideKeyboard(){
@@ -38,7 +36,13 @@ public class TestBase {
 	}
 	
 	public void swipeUpInBatchList(){	
-		touchAction.longPress(390,670).moveTo(390,430).release().perform();
+		
+		Dimension size = driver.manage().window().getSize();
+		int startx = size.width / 2 ;
+		int starty = (int) (size.height * 0.8) ;
+		int endy = (int) (size.height * 0.2) ;
+		driver.swipe(startx, starty, startx, endy, 3000);
+		/*touchAction.longPress(390,670).moveTo(390,430).release().perform();*/
 	}
 
 }
