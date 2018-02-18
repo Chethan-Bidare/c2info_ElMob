0c501b2f94471844750c01071ede01cd39d219dd
Chethan-Bidare <31341016+Chethan-Bidare@users.noreply.github.com>
2018-02-18 12:04:54 +0530

    check

---
+++ src/main/java/c2info_ElMob/c2info_ElMob/App.java
@@ -0,0 +1,13 @@
+package c2info_ElMob.c2info_ElMob;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
